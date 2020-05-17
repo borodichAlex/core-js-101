2ec2204db1f51b664a1aee79a5ca46242c8467b3
Student <borodichalexander97@gmail.com>
2020-05-18 00:00:58 +0300

    feat: add 09-functions

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -81,8 +81,13 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let store = null;
+
+  return () => {
+    if (store == null) { store = func(); }
+    return store;
+  };
 }
 
 
